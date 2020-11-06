4a41a56165e7cedb25ddd4ddc23e4de0ed23bc6a
Oscar <oscar20azm@gmail.com>
2020-11-05 19:51:11 -0600

    _

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -27,6 +27,16 @@
  let deck       = [];
  let types      = ['C','D','S','H'];
  let especials  = ['A','J','Q','K'];
+ let playerPoints = 0,
+     computerPoints = 0;
+
+ const btnNewGame = document.querySelector("#btnNewGame");
+ const btnGetCard = document.querySelector("#btnGetCard");
+ const btnFinishGame = document.querySelector("#btnFinishGame");
+ const playerPointsLabel = document.querySelector('#playerPoints');
+ const computerPointsLabel = document.querySelector('#computerPoints');
+ const playerCards = document.querySelector('#playerCards');
+ const computerCards = document.querySelector('#computerCards');
 
  const createDeck = () => {
 
@@ -52,7 +62,6 @@
      */
 
     deck = _.shuffle(deck);
-    console.log(deck);
     return deck;
 }
 
@@ -62,17 +71,116 @@ createDeck();
  * Función que nos devuelve una carta
  */
 
- const giveCard = () => {
+ const getCard = () => {
 
     if(deck.length === 0){
         throw 'No hay cartas en el deck';
     }
 
     const card = deck.pop();
-
-    console.log(card);
-    console.log(deck);
     return card;
  }
 
- giveCard();
+ const getCardValue = (card) => {
+    const cardValue = card.substring(0, card.length -1);
+    return (isNaN(cardValue)) ? 
+            (cardValue === 'A') ? 11 : 10
+            :cardValue * 1;
+ }
+
+ /** Turno de la computadora */
+const computerTurn = (minimunPoints) => {
+
+   do {
+    const newCard = getCard();
+    computerPoints  = computerPoints + getCardValue(newCard);
+    computerPointsLabel.innerText = computerPoints;
+
+    const printCard = document.createElement('img');
+    printCard.src = `assets/cartas/${newCard}.png`;
+    printCard.classList.add('cardImage');
+    computerCards.append(printCard);
+
+    if(minimunPoints > 21){
+        break;
+    }
+
+   }while(computerPoints < minimunPoints && minimunPoints <= 21);
+
+   setTimeout(()=>{
+    if(computerPoints === minimunPoints){
+        Swal.fire(
+            '¡Nadie gana!',
+            '¿Quieres jugar de nuevo?',
+            'warning'
+            )
+    }else if(minimunPoints > 21){
+        Swal.fire(
+            '¡Perdiste!',
+            'La computadora gana, ¿Quieres jugar de nuevo?',
+            'error'
+            )
+    }else if(computerPoints > 21){
+        Swal.fire(
+            '¡Ganaste!',
+            '¿Quieres jugar de nuevo?',
+            'success'
+            )
+    }else if(computerPoints > minimunPoints && computerPoints < 21){
+        Swal.fire(
+            '¡Perdiste!',
+            'La computadora gana, ¿Quieres jugar de nuevo?',
+            'error'
+            )
+    }else{
+        Swal.fire(
+            '¡Perdiste!',
+            'La computadora gana, ¿Quieres jugar de nuevo?',
+            'error'
+            )
+    }
+   }, 10);
+}
+
+/**
+ *                               EVENTOS
+ */
+
+ btnGetCard.addEventListener('click', () => {
+
+    const newCard               = getCard();
+    playerPoints                = playerPoints + 
+                                    getCardValue(newCard);
+    playerPointsLabel.innerText = playerPoints;
+
+    const printCard     = document.createElement('img');
+    printCard.src       = `assets/cartas/${newCard}.png`;
+    printCard.classList.add('cardImage');
+
+    playerCards.append(printCard);
+
+    if(playerPoints > 21){
+        btnGetCard.disabled = true;       
+        computerTurn(playerPoints);
+        //btnNewGame.classList.remove('newGameButton'); Remover una clase css
+    } else if (playerPoints === 21){
+        btnGetCard.disabled = true;
+        Swal.fire(
+            '¡21, Genial!',
+            '¡Espera el turno de la computadora!',
+            'error'
+            )       
+        computerTurn(playerPoints);
+    }
+
+ });
+
+ btnFinishGame.addEventListener('click', ()=> {
+    btnGetCard.disabled     = true;
+    btnFinishGame.disabled  = true;
+    computerTurn(playerPoints);
+ });
+
+ btnNewGame.addEventListener('click', () => {
+
+ });
